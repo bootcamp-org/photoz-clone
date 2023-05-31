4050e1b580364156afa4cfa27dcdf9b94e94cb03
Mateus Abelli <mateusabelli@gmail.com>
2023-05-30 21:03:58 -0300

    Initial commit

---
+++ src/test/java/io/github/mateusabelli/photozclone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusabelli.photozclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
